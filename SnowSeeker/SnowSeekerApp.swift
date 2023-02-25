17829874d7c6dcf204e9854000711186f7238f9b
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-25 14:35:25 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Edwin Przeźwiecki Jr. on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
